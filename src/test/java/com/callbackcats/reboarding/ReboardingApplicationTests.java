208cd859f179c551f63ad24e5c55985fdd02811e
Pecske <pecske92@gmail.com>
2020-06-11 14:22:37 +0200

    add test hierarchy add application.yaml

---
+++ src/test/java/com/callbackcats/reboarding/ReboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.callbackcats.reboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
