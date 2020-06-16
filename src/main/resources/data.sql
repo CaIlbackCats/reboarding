5e4ed7c51cf35195d0e786b976a5b305b77d88db
Pecske <pecske92@gmail.com>
2020-06-16 11:38:46 +0200

    fix data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,17 +7,6 @@ VALUES (1,250,'2020-06-01','2020-06-30');
 INSERT INTO office_capacity(id,capacity_limit,start_date,end_date)
 VALUES (2,0,'2020-07-01','2020-07-31');
 
--- INSERT INTO employee (id,in_office)
--- VALUES ('0',false);
--- INSERT INTO employee (id,in_office)
--- VALUES ('1',false);
--- INSERT INTO employee (id,in_office)
--- VALUES ('2',false);
--- INSERT INTO employee (id,in_office)
--- VALUES ('3',false);
--- INSERT INTO employee (id,in_office)
--- VALUES ('4',false);
-
 INSERT INTO reservation (id,r_date,reservation_type,capacity_id)
 VALUES (0,CURRENT_DATE,'RESERVED',(SELECT id FROM office_capacity WHERE CURRENT_DATE >= start_date AND CURRENT_DATE <=end_date));
 INSERT INTO reservation(id,r_date,reservation_type,capacity_id)
@@ -29,7 +18,7 @@ VALUES (3,'2020-06-02','RESERVED',(SELECT id FROM office_capacity WHERE '2020-06
 
 
 INSERT INTO employee_reservation (reservation_id,employee_id)
-VALUES ((SELECT id FROM reservation WHERE r_date = CURRENT_DATE AND reservation_type = 'RESERVED'),'1');
+VALUES ((SELECT id FROM reservation WHERE r_date = CURRENT_DATE AND reservation_type = 'RESERVED'),'0');
 INSERT INTO employee_reservation (reservation_id,employee_id)
 VALUES ((SELECT id FROM reservation WHERE r_date = '2020-06-02'),'2');
 INSERT INTO employee_reservation (reservation_id,employee_id)
@@ -37,11 +26,11 @@ VALUES ((SELECT id FROM reservation WHERE r_date = '2020-06-02'),'3');
 INSERT INTO employee_reservation (reservation_id,employee_id)
 VALUES ((SELECT id FROM reservation WHERE r_date = '2020-06-02'),'4');
 INSERT INTO employee_reservation(reservation_id,employee_id)
-VALUES (1,1);
+VALUES (1,0);
 INSERT INTO employee_reservation(reservation_id,employee_id,permission_to_office)
-VALUES (1,3,true);
+VALUES (1,2,true);
 INSERT INTO employee_reservation(reservation_id,employee_id,permission_to_office)
-VALUES (1,4,false);
+VALUES (1,3,false);
 
 
 
