1aaeffdbf5bd64506d65aaa9afbf37fb63e8e01b
Hudák Elza <hudak.elza@gmail.com>
2020-06-23 01:02:54 +0200

    add chair templates

--- src/main/java/com/callbackcats/reboarding/util/TemplateMatcher.java
+++ src/main/java/com/callbackcats/reboarding/util/TemplateMatcher.java
@@ -1,64 +1,65 @@
 package com.callbackcats.reboarding.util;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
+import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+
 public class TemplateMatcher {
+    private static Mat dst;
+    private static Set<Point> templates = new HashSet<>();
+
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
 
     public static void main(String[] args) {
 
-//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-//        Mat source = null;
-//        Mat template = null;
-//        //Load image file
-//        source = Imgcodecs.imread("office_layout.jpg");
-//        template = Imgcodecs.imread("chair.jpg");
-//
-//        Mat outputImage = new Mat();
-//        int machMethod = Imgproc.TM_CCOEFF_NORMED;
-//        //Template matching method
-//        Imgproc.matchTemplate(source, template, outputImage, machMethod);
-//
-//
-//        Core.MinMaxLocResult mmr = Core.minMaxLoc(outputImage);
-//        Point matchLoc = mmr.maxLoc;
-//        //Draw rectangle on result image
-//        Imgproc.rectangle(source, matchLoc, new Point(matchLoc.x + template.cols(),
-//                matchLoc.y + template.rows()), new Scalar(0, 0, 255));
-//
-//        Imgcodecs.imwrite("modified_office_layout.jpg", source);
-//        System.out.println("Completed.");
-
-
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat img = Imgcodecs.imread("office_layout.jpg");
-        Mat tpl = Imgcodecs.imread("chair_02.jpg");
+        dst = img.clone();
+        Imgproc.GaussianBlur(img, dst, new Size(0, 0), 10);
+        Core.addWeighted(img, 1.5, dst, -0.5, 0, dst);
+        for (int i = 0; i < 12; i++) {
+            String templatePath = "src/main/resources/img_templates/chair_" + i + ".jpg";
+            signTemplate(templatePath, img);
+        }
+        Imgcodecs.imwrite("modified_office_layout.jpg", dst);//save image
+        System.out.println(templates.size() + " chairs found.");
+        System.out.println(templates);
+    }
+
+
+    private static void signTemplate(String templatePath, Mat img) {
+        Mat tpl = Imgcodecs.imread(templatePath);
         Mat result = new Mat();
         Imgproc.matchTemplate(img, tpl, result, Imgproc.TM_CCOEFF_NORMED);
         Imgproc.threshold(result, result, 0.1, 1, Imgproc.THRESH_TOZERO);
-        double threshold = 0.65;
+        double threshold = 0.57;
         double maxval;
-        Mat dst;
         while (true) {
             Core.MinMaxLocResult maxr = Core.minMaxLoc(result);
             Point maxp = maxr.maxLoc;
             maxval = maxr.maxVal;
-            Point maxop = new Point(maxp.x + tpl.width(), maxp.y + tpl.height());
             dst = img.clone();
             if (maxval >= threshold) {
-
                 Imgproc.rectangle(img, maxp, new Point(maxp.x + tpl.cols(),
                         maxp.y + tpl.rows()), new Scalar(0, 0, 255), 5);
                 Imgproc.rectangle(result, maxp, new Point(maxp.x + tpl.cols(),
                         maxp.y + tpl.rows()), new Scalar(0, 0, 255), -1);
+                templates.add(maxp);
             } else {
                 break;
             }
         }
-        Imgcodecs.imwrite("modified_office_layout.jpg", dst);//save image
+    }
+
+    //TODO
+    private static boolean isAlreadyInTemplateSet(Point maxp) {
+        Optional<Point> maybePoint = templates.stream().filter(p -> Math.abs(maxp.x - p.x) < 3 && Math.abs(maxp.y - p.y) < 3).findFirst();
+        return maybePoint.isPresent();
     }
 }
